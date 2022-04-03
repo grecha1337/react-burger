3dee96042dac7294960aa0f07a3664d496b92b70
grecha1337 <4rmor3d@gmail.com>
2022-04-03 23:36:12 +0300

    Правки

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -61,7 +61,7 @@ const App: FC = () => {
 
   const orderNumber = useRouteMatch<{ number?: string | undefined }>([
     "/feed/:number",
-    "profile/orders/:number"
+    "/profile/orders/:number"
   ])?.params?.number;
 
   return (
